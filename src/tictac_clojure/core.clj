2302dd1e2e630c98f7545cafb9b76973e52ddf5e
yinnie <yinnie.yinnie@gmail.com>
2013-08-06 01:06:08 -0400

    added minimax

--- src/tictac_clojure/core.clj
+++ src/tictac_clojure/core.clj
@@ -3,66 +3,118 @@
 
 (defn full? [board] 
   (cond
-    ((not-any? #{0} board) true)
-    false))
+    (not-any? #{0} board) true
+    :else false))
+
+(defn sel-three [v x y z]
+  (vector (nth v x) (nth v y) (nth v z)))
 
 (defn make-triples [board]
   ;;turn the board vector into combinations of winning pos
-  (let [diagnal1 (vector (nth board 0) (nth board 4) (nth board 8))
-        diagnal2 (vector (nth board 2) (nth board 4) (nth board 6))]
-    (cons diagnal1 (cons diagnal2 (partition 3 board)))))
+  (let [diagnl1 (sel-three board 0 4 8) 
+        diagnl2 (sel-three board 2 4 6)
+        vert1   (sel-three board 0 3 6)
+        vert2   (sel-three board 1 4 7)
+        vert3   (sel-three board 2 5 8)
+        hor1    (sel-three board 0 1 2) 
+        hor2    (sel-three board 3 4 5)
+        hor3    (sel-three board 6 7 8)]
+        (vector diagnl1 diagnl2 vert1 vert2 vert3 hor1 hor2 hor3)))
         
 (defn same? [triplet]
   (cond
-    ((every? #{2} triplet) true)
-    ((every? #{1} triplet) true)
+    (every? #{2} triplet) true
+    (every? #{1} triplet) true
     :else false))
   
 (defn contain-same? [triples]
   (let [results 
         (for [t triples]
-          ((same? t) true))]
-    (or results)))
+          (same? t))]
+        (contains? (set results) true)))
 
-(defn check-win [board] 
+(defn win? [board] 
   (let [triples (make-triples board)]
-    (contain-same? triples)))
+       (contain-same? triples)))
 
 (defn empty-cells [board]
   ;;indexes of board that are empty
   (let [indexed (map-indexed vector board)]
-    (let [filtered (filter #(= 0 (second %)) indexed)]
-      (map first filtered))))
+        (let [filtered (filter #(= 0 (second %)) indexed)]
+              (map first filtered))))
 
 (defn choose-rand [board symbl]
   (let [cells (empty-cells board)]
-    (assoc (rand-nth cells) symbl)))
+       (assoc board (rand-nth cells) symbl)))
 
-(defn computer [board my-symbol]
+(defn end? [board]
+  (or (win? board) (tie? board)))
+
+(defn get-winner [board]
+  )
+
+(defn leaf-val [board]
   (cond
-    ((full? board) (check-win board))
-    (choose-rand board my-symbol)))
+    (win? board) (let [winner (get-winner board)]
+                      (case winner
+                        "human"   -1
+                        "computer" 1))
+    :else 0))  
+                      
+(defn get-poss-boards [board symbl]
+  (let [cells (empty-cells board)]
+       (for [cell cells]
+         (assoc board cell symbl)))) 
 
-(defn valid? [text]
-  ;;TODO modify so between 0-8. that cell free
-  (if (number? (read-string text)) true false))
+(defn get-board [op boards sym depth]
+;;returns best-move board
+  (reduce (fn [ x y]
+            (let [xval (minimax x sym (inc depth))
+                  yval (minimax y sym (inc depth))]
+              (case op
+                "max" (if (> yval xval) y x)
+                "min" (if (< yval xval) y x))))
+                                        boards))
 
-(defn input []
+(defn minimax [board sym depth]
+  (cond
+    (end? board) (leaf-val board)
+    :else (let [boards (get-poss-boards board sym)]
+               (case sym 
+                 1 (get-board "max" boards 2 depth)
+                 2 (get-board "min" boards 1 depth))))) 
+
+(defn computer [board symbl]
+  (minimax board symbl 0))
+
+(defn tie? [board]
+  (if (empty? (empty-cells board)) true false))
+
+(defn valid? [input board]
+  (let [s (read-string input)]
+       (and (number? s) (contains? (set (empty-cells board)) s))))  
+
+(defn input [board]
   (println "enter a number between 1 and 9 inclusive")
-  (if-let [words (valid? (read-line))]
-    words
-    (println "oops invalid input. come again")))
+  (let [s (read-line)]
+  (cond 
+    (valid? s board) (read-string s) 
+    :else "oops invalid input. try again")))
 
-(defn human [board my-symbol]
-  (cond
-    ((nil? (input)) (human board my-symbol))
-    (let [cell (input)]
-      (assoc cell my-symbol))))
+(defn human [board symbl]
+  (let [cell (input board)]
+    (cond
+     (not (number? cell)) (human board symbl)
+     :else (assoc board cell symbl))))
 
 (defn play [board]
-  (let [curboard (computer board 1)]
-    (println curboard)
-    (recur (human curboard 2))))
+  (println board)
+  (cond
+    (win? board) "you won!!"
+    (tie? board) "it's a tie"
+    :else (let [curboard (computer board 1)]
+               (println curboard)
+               (recur (human curboard 2)))))
 
 (defn -main []
   (play [0 0 0 0 0 0 0 0 0]))
